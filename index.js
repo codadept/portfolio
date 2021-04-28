5655011c4f9b8f2854b00bb6c5fa8ebdd4ab4c2d
codadept <info.pratikmajumdar@gmail.com>
2021-04-29 00:20:22 +0530

    add basic directory and requried files

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-var express = require("express");
-var app = express();
-var PORT = 3000;
+const express = require("express");
+const app = express();
+const path =  require('path');
+const PORT = 3000;
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'/views'));
+
+app.use(express.static(path.join(__dirname,'/public')));
 
 app.get('/',(req,res)=>{
-    res.send("HI");
+    const num = Math.floor(Math.random()*100)+1;
+    res.render('home',{rand:num});
 })
 
 app.listen(PORT,()=>{
