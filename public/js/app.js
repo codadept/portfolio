2743d33e9a7b754333ab34bb73979c35ea8dd139
codadept <info.pratikmajumdar@gmail.com>
2021-05-01 12:32:18 +0530

    update mobile view homepage

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,15 @@
 var navLinks = document.querySelector("header .nav-links");
 var close = document.querySelector(".close");
 var ham = document.querySelector(".ham");
-var scrollToTop = document.querySelector("#btnScrollToTop");
 
 ham.addEventListener("click",()=>{
     navLinks.style.right = "0";
-    close.style.right = "100px";
+    close.style.right = "150px";
     scrollToTop.style.display="none";
 })
 
 close.addEventListener("click",()=>{
-    close.style.right="-100px";
-    navLinks.style.right="-150px";
+    close.style.right="-150px";
+    navLinks.style.right="-200px";
     scrollToTop.style.display="block";
 })
