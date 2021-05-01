d2b1d412d83b658c1965c37db3d63a8d86b532ba
codadept <info.pratikmajumdar@gmail.com>
2021-05-01 11:54:08 +0530

    scroll button updated

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,16 @@
 var navLinks = document.querySelector("header .nav-links");
 var close = document.querySelector(".close");
 var ham = document.querySelector(".ham");
+var scrollToTop = document.querySelector("#btnScrollToTop");
 
 ham.addEventListener("click",()=>{
     navLinks.style.right = "0";
     close.style.right = "100px";
+    scrollToTop.style.display="none";
 })
 
 close.addEventListener("click",()=>{
     close.style.right="-100px";
     navLinks.style.right="-150px";
+    scrollToTop.style.display="block";
 })
