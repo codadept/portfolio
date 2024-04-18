face0de7f31be98d31302d9de21befe41ba99e3d
Pratik Majumdar <info.pratikmajumdar@gmail.com>
2024-04-18 21:31:45 +0530

    Add initial configuration

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,7 @@
+import styles from "./Home.module.scss";
+
+const Home = () => {
+    return <h1 className={styles["heading"]}>Home</h1>;
+};
+
+export default Home;
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Home from "./Home/Home"
+import Blogs from "./Blogs/Blogs"
+import Projects from "./Projects/Projects"
+
+export {
+  Home,
+  Blogs,
+  Projects
+}
