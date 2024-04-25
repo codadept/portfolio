349367613df4b36780900ec7d962b795facb9e71
Pratik Majumdar <info.pratikmajumdar@gmail.com>
2024-04-26 00:13:08 +0530

    Complete remarks section

--- src/components/Remarks/Remarks.tsx
+++ src/components/Remarks/Remarks.tsx
@@ -1,12 +1,21 @@
+import Link from "../Link/Link";
+
 import styles from "./Remarks.module.scss";
 
 const Remarks: React.FC = () => {
   return (
     <article className={styles["remarks"]}>
+      <p>&#169; 2024 Pratik Majumdar. All rights reserved.</p>
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda
-        corporis vero id aliquid beatae consequatur accusamus ab earum omnis
-        aperiam.
+        Crafted in <Link to="https://figma.com/">Figma</Link> and developed with{" "}
+        <Link to="https://react.dev/">React</Link>,{" "}
+        <Link to="https://vitejs.dev/">Vite</Link>, and{" "}
+        <Link to="https://www.typescriptlang.org/">TypeScript</Link> in{" "}
+        <Link to="https://code.visualstudio.com/">Visual Studio Code</Link>,
+        enhanced with <Link to="https://sass-lang.com/">Sass</Link> for styling.
+        Hosted on <Link to="https://vercel.com/">Vercel</Link>, with the text
+        font being{" "}
+        <Link to="https://fonts.google.com/specimen/Fira+Mono">Fira Mono</Link>.
       </p>
     </article>
   );
